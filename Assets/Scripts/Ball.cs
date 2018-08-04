28905e9d5f6589ad7917c3c4c37d4687b1ab2d1f
MartinVN <m4rtini89@gmail.com>
2018-08-04 18:00:54 +0200

    Lecture 59

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Vector2 launchSpeed = new Vector2(2f, 15f);
+    [SerializeField] GameObject glueGO;
+
+    Vector3 glueOffset;
+    bool isGlued = true;
+	// Use this for initialization
+	void Start () {
+        glueOffset = transform.position - glueGO.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isGlued)
+        {
+            LockBallToObject();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var rigidBody = GetComponent<Rigidbody2D>();
+            rigidBody.velocity = launchSpeed;
+            isGlued = false;
+        }
+    }
+
+    private void LockBallToObject()
+    {
+        transform.position = glueGO.transform.position + glueOffset;
+    }
+}
--- Assets/Scripts/PaddleMovement.cs
+++ Assets/Scripts/PaddleMovement.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour {
-    [SerializeField]
-    private float screenWidthInUnits = 16f;
-
+    // Configuration variables
+    [SerializeField]private float screenWidthInUnits = 16f;
+    [SerializeField] float paddleXMin = 1f;
+    [SerializeField] float paddleXMax = 15f;
 
     // Update is called once per frame
     void Update () {
         var mouseX = Input.mousePosition.x;
         var mouseXRelative = mouseX / Screen.width;
         var mouseXWorld = mouseXRelative * screenWidthInUnits;
+        mouseXWorld = Mathf.Clamp(mouseXWorld, paddleXMin, paddleXMax);
 
         transform.position = new Vector3(mouseXWorld, transform.position.y, transform.position.z);
 
