33a0c8872c2146a457d4db8cc92d03e62a55f5db
MartinVN <m4rtini89@gmail.com>
2018-08-04 12:16:44 +0200

    Lecture 52 - colliders and movement

---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+    [SerializeField]
+    private float screenWidthInUnits = 16f;
+
+
+    // Update is called once per frame
+    void Update () {
+        var mouseX = Input.mousePosition.x;
+        var mouseXRelative = mouseX / Screen.width;
+        var mouseXWorld = mouseXRelative * screenWidthInUnits;
+
+        transform.position = new Vector3(mouseXWorld, transform.position.y, transform.position.z);
+
+	}
+}
